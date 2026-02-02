96d7156d7ee84c547c310e766bf36ecf59fd32b8
Crisoxyz <cristian.torre.xyz@gmail.com>
2026-02-02 16:31:20 +0100

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // Sostituisci con i tuoi dati reali!
 export default defineConfig({
   // Metti qui l'URL completo che avrai
-  site: 'https://TUO_USERNAME.github.io', 
+  site: 'https://crisoxyz.github.io', 
   // Metti qui il nome della tua repository (es. /portfolio-2026)
   base: '/Portfolio2026', 
   
